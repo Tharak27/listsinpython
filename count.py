c9c67ccd489a35c0309690dd9c4caeb6ec2a8dde
Tharak Ram <tharakarame@gmail.com>
2026-05-06 10:12:12 +0530

    Add count.py to count occurrences of an item

---
+++ count.py
@@ -0,0 +1,6 @@
+#count
+numbers=[1,0,4,1,5,0,3,5,2,6,4,5,]
+print(f"The list is:{numbers}")
+item_to_count=int(input("Enter the item to be counted:"))
+c=numbers.count(item_to_count)
+print(f"The count of {item_to_count} is {c}")
