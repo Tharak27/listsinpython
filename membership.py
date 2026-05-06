046fe2d4282d42e694e9a771436a4efb5897f284
Tharak Ram <tharakarame@gmail.com>
2026-05-06 10:16:54 +0530

    Add membership operation examples in membership.py

---
+++ membership.py
@@ -0,0 +1,8 @@
+#membership operation
+#in
+languages=['python','java','c++']
+print('python' in languages)
+print('javascript' in languages)
+#not in
+print('python' not in languages)
+print('javascript' not in languages)
